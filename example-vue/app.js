a9cce244ac849f0c644044e83d3e35d3eaa4652d
Francisco De Cozar Lopez <francisco.cozar@altran.es>
2017-09-05 14:28:01 +0200

    new

---
+++ example-vue/app.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+    el: '#app',
+    template: `
+        <div class="view">
+            <game-header></game-header>
+            <game-add @new="addNewGame"></game-add>
+            <game-list v-bind:games="games"></game-list>
+        </div>
+    `,
+    data: {
+        games: [
+            { title: 'ME: Andromeda' },
+            { title: 'Fifa 2017' },
+            { title: 'League of Legend' }
+        ]
+    },
+    methods: {
+        addNewGame: function (game) {
+            this.games.push(game);
+        }
+    }
+});
